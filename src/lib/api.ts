e58910cee5c038bd8d31cb596482db16fe10b586
basumadhuboni <basumadhuboni@gmail.com>
2025-10-07 17:43:43 +0530

    initial commit

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,9 +4,6 @@ const API_URL =  'http://localhost:4000';
 
 export const api = axios.create({
   baseURL: API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
 });
 
 api.interceptors.request.use((config) => {
